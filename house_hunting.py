8cc9f1dc41fecb3c5a3cdf4eba9c708293cee633
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-18 21:41:05 -0500

    Final final

--- house_hunting.py
+++ house_hunting.py
@@ -1,4 +1,5 @@
 # inputs from the user
+print("Want to know how many months will you have to save money to get a down payment for a home?")
 annual_salary = int(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = int(input("Enter the cost of your dream home: "))
