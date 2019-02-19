8aca57e17495f74938c793b14af00ba6950e2aa4
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-18 21:27:27 -0500

    finished product

--- house_hunting.py
+++ house_hunting.py
@@ -1,7 +1,38 @@
-total_cost = ()
-portion_down_payment = 0.25
-current_savings = ()
-current_savings*r/12
-r = 0.04
-annual_salary​ = input()
-portion_saved = ()
+# inputs from the user
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
+percentage_down = input("Enter the percent of your home's cost to save as a down payment [0.25]: ")
+rate = input("Enter the expected annual rate of return [0.04]: ")
+
+# values to be calculated
+
+if rate == '':
+    rate = .04
+else:
+    rate = float(rate)
+if percentage_down == '':
+    percentage_down = .25
+else:
+    percentage_down = float(percentage_down)
+
+portion_down_payment = total_cost * percentage_down
+
+# current_savings must be set to zero, because it starts gaining money after month 1.
+
+current_savings = 0
+monthly_salary = annual_salary / 12
+
+# months starts at zero, and will count up from there until reaching the monetary amount needed in the rest of the program. 
+
+months = 0
+monthly_saving = monthly_salary * portion_saved
+
+#saving up for down payment
+
+while current_savings < portion_down_payment:
+    investments = current_savings*rate/12
+    current_savings = monthly_saving + investments + current_savings
+    months += 1
+
+print("Number of months: " + str(months))
